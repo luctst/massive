5e7ace94cd9f36dacacc877e1f413f4da9d0872c
luctst <lucas.tostee@gmail.com>
2023-02-14 18:32:55 +0100

    feat: update header when user is on /article/:id route

--- apps/client/src/components/navigation-header.vue
+++ apps/client/src/components/navigation-header.vue
@@ -2,15 +2,28 @@
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 
-const title = ref<string>(useRoute().meta.headerTitle as string);
+const title = ref<string>(useRoute().meta.headerTitle as string || '');
 </script>
 
 <template>
   <header class="container">
-    <h1>{{ $t(title) }}</h1>
-    <div class="is__container__img">
-      <img src="@/assets/icon_filter.svg">
-    </div>
+    <template v-if="$route.name === 'Article'">
+      <div class="is__container__img">
+        <img
+          src="@/assets/Union-black.svg"
+          @click="$router.back()"
+        >
+      </div>
+      <div class="is__container__img">
+        <img src="@/assets/Setting-black.svg">
+      </div>
+    </template>
+    <template v-else>
+      <h1>{{ $t(title) }}</h1>
+      <div class="is__container__img">
+        <img src="@/assets/icon_filter.svg">
+      </div>
+    </template>
   </header>
 </template>
 
