dd2a9ce26e5333abf9db338d32d2a1c19f843598
luctst <lucas.tostee@gmail.com>
2023-02-14 18:34:30 +0100

    feat: mock data to auth user

--- apps/client/src/stores/user.ts
+++ apps/client/src/stores/user.ts
@@ -4,9 +4,11 @@ import { UserStore } from '@/types/index';
 export const useUserStore = defineStore({
   id: 'user',
   state: (): UserStore => ({
+    id: 0,
     isConnected: true,
     firstname: '',
     lastname: '',
     avatar: null,
+    bookmarks: null,
   }),
 });
